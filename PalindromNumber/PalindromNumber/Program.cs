f3ac2810ed9d6c8da329fde685300871b2fa78eb
kamolbekb <118444680+kamolbekb@users.noreply.github.com>
2023-07-27 11:41:44 +0500

    Tested

---
+++ PalindromNumber/PalindromNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number:");
+            int input = int.Parse(Console.ReadLine());
+            IsPalindrom(input);
+        }
+        static void IsPalindrom(int n)
+        {
+            int newN = n, r, sum = 0; ;
+            while (n > 0)
+            {
+                r = n % 10;
+                sum = (sum * 10) + r;
+                n = n / 10;
+            }
+            if (sum == newN)
+            {
+                Console.WriteLine("Entered number is Palindrom!");
+            }
+            else
+            {
+                Console.WriteLine("Entered number isn`t Palindrom!");
+            }
+        }
+    }
+}
